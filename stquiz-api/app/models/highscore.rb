3142ad7f9fa0633b602a3c5529ac6573e33448db
Syd <24235464+sydeast@users.noreply.github.com>
2021-04-23 13:45:46 -0500

    "Update spec"

--- stquiz-api/app/models/highscore.rb
+++ stquiz-api/app/models/highscore.rb
@@ -1,9 +1,10 @@
 class Highscore < ApplicationRecord
   belongs_to :quiz
 
-    def highest_score
-      self.order(score: :desc).limit(20)
-    end
+ 
+    # def highest_score
+    #   self.order(score: :desc).limit(20)
+    # end
 
 
 
